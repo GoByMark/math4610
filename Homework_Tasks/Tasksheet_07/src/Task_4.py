0acf77da13ea1ad8c12064d2e9fb757ddb531196
Xiang Gao <89796613+GoByMark@users.noreply.github.com>
2021-11-19 03:06:44 -0700

    Update Task_4.py

--- Homework_Tasks/Tasksheet_07/src/Task_4.py
+++ Homework_Tasks/Tasksheet_07/src/Task_4.py
@@ -18,7 +18,7 @@ x = [1 for i in range(n)]
 b = [1 for i in range(n)]
 
 if np.linalg.det(inputMatrix) != 0:
-    x = np.matmul(np.linalg.inv(inputMatrix), b)
+    x = np.dot(np.linalg.inv(inputMatrix), b)
     print("The solution of the given diagonal matrix with result b_i = 1 is, x = ", x)
 else:
     print("Please pick a better n next time!")
