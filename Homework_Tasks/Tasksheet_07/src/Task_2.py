e64f97be2d525fc21890fa66c6ceed76701fa83e
Xiang Gao <89796613+GoByMark@users.noreply.github.com>
2021-11-19 02:48:13 -0700

    Add files via upload

---
+++ Homework_Tasks/Tasksheet_07/src/Task_2.py
@@ -0,0 +1,25 @@
+import numpy as np
+from matrixL import matrixLame
+
+np.set_printoptions(precision=4)
+
+# ask the user for the size of the matrix
+
+n = int(input('Please give the rows of this lower triangular matrix: '))
+m = int(input('Please give the columns of this lower triangular matrix: '))
+
+# set up the matrix and what not call it inputMatrix?
+inputMatrix = matrixLame(n, m)
+
+# to find the solution, probably want to check the determinant first just in case
+
+# initialize the solution and the result vector
+
+x = [1 for i in range(m)]
+b = [1 for i in range(n)]
+
+if np.linalg.det(inputMatrix) != 0:
+    x = np.matmul(np.linalg.inv(inputMatrix), b)
+    print("The solution of the given lower triangular matrix with result b_i = 1 is,\n x =", x)
+else:
+    print("Please pick a better n and m next time!")
