eaba1ef73b33a385806cc8b654a443add9e93a33
Xiang Gao <89796613+GoByMark@users.noreply.github.com>
2021-12-16 05:22:06 -0700

    Update DotPro.py

--- Homework_Tasks/Tasksheet_09/src/DotPro.py
+++ Homework_Tasks/Tasksheet_09/src/DotPro.py
@@ -1,8 +1,7 @@
-import numpy as np
-
 def DotPro(arr1, arr2):
     if len(arr1) != len(arr2):
         print('two vectors are not the same length')
-
-    DotPro = np.dot(arr1, arr2)
-    return DotPro
+    sum = 0
+    for i in range(len(arr1)):
+        sum += arr1[i] * arr2[i]
+    return sum
