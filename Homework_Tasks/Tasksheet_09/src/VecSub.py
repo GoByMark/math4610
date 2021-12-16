ff53742ed451e2e6b899d697254072ec41006706
Xiang Gao <89796613+GoByMark@users.noreply.github.com>
2021-12-16 05:27:33 -0700

    Update VecSub.py

--- Homework_Tasks/Tasksheet_09/src/VecSub.py
+++ Homework_Tasks/Tasksheet_09/src/VecSub.py
@@ -1,5 +1,5 @@
-import numpy as np
-
 def VecSub(arr1, arr2):
-    diff = np.subtract(arr1, arr2)
+    diff = []
+    for i in range(len(arr1)):
+        diff.append(arr1[i] + arr2[i])
     return diff
